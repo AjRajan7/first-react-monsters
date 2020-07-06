68c3e29173d6a396e9d048936d2c5f0e1f59dea0
AjRajan7 <newacct7robot@gmail.com>
2020-07-06 15:10:28 +0530

    Updates

---
+++ precache-manifest.76eb69f6f856771a11c0df9eb256dfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571a7e84ca977adccbf93825a8a4eb35",
+    "url": "/first-react-monsters/index.html"
+  },
+  {
+    "revision": "bfb3ed0cafb48f3e1013",
+    "url": "/first-react-monsters/static/css/main.51bd465e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/first-react-monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb3ed0cafb48f3e1013",
+    "url": "/first-react-monsters/static/js/main.a48de310.chunk.js"
+  },
+  {
+    "revision": "af23e09a74f92b31b67c",
+    "url": "/first-react-monsters/static/js/runtime-main.9fee5604.js"
+  }
+]);
